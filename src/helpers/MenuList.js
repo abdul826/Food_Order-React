eeff4834d1d6a05a7c6e0f096d4d295954e2098b
abdul826 <59992187+abdul826@users.noreply.github.com>
2022-02-14 12:09:06 +0530

    Add files via upload

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,42 @@
+import Chole from "../assets/chhola.jpg";
+import Dosa from "../assets/dosa.jpg";
+import Paneer from "../assets/paneer.jpg";
+import Burger from "../assets/burger.jpg";
+import Gujrati from "../assets/gujrati.jpeg";
+import Rajasthani from "../assets/rajasthani.jpg";
+
+export const MenuList = [
+  {
+    name: "Chole Bhature",
+    image: Chole,
+    price: 250,
+  },
+  {
+    name: "Masala Dosa",
+    image: Dosa,
+    price: 280,
+  },
+  {
+    name: "Butter Paneer",
+    image: Paneer,
+    price: 350,
+  },
+  {
+    name: "Burger",
+    image: Burger,
+    price: 230,
+  },
+  {
+    name: "Gujrati Thali",
+    image: Gujrati,
+    price: 450,
+  },
+  {
+    name: "Rajasthani",
+    image: Rajasthani,
+    price: 499,
+  },
+  
+];
+
+export default MenuList;
